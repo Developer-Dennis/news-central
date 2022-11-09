8e5de107a55f18b7be1bdf8be8542ccfe858f6ea
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-09 14:39:43 +0300

    created the navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <ul>
+  <li><a class="active" href="#newscentral">News Central</a></li>
+  <li><a href="#politics">Politics</a></li>
+  <li><a href="#business">Business</a></li>
+  <li><a href="#entartainment">Entertainment</a></li>
+  <li><a href="#sports">Sports</a></li>
+</ul>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+ul {
+  margin: 0 5px;
+  width: 100%;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+
+</style>
